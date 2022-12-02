86af3996410ba30a8ed760d73e60cae7b18b4671
irfanonk <onk.irfan@gmail.com>
2022-12-02 17:10:20 +0300

    baseUrl

--- src/features/api/api.ts
+++ src/features/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api/"
+    baseURL: "https://e-commerce-jsondb.vercel.app/"
 })
 
 api.interceptors.request.use((config) => {
