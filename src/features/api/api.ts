f74e78df62de6e0e239ea8466962a65f929f759d
irfanonk <onk.irfan@gmail.com>
2022-12-02 17:11:45 +0300

    change api

--- src/features/api/api.ts
+++ src/features/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: "https://e-commerce-jsondb.vercel.app/"
+    baseURL: "https://kijabi-be.vercel.app/api/"
 })
 
 api.interceptors.request.use((config) => {
